f3131470d33f913ee1865105d79eddb649e32e22
PedroPeriquito <pedro_perikito@hotmail.com>
2023-09-18 14:25:48 +0100

    App e Index incompleto

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import './navbar.css';
+
+const Navbar = () => {
+	return (
+		<div className='navbar'>
+			<div className='navContainer'>
+                <span className='logo'>Nature Adventure</span>
+                <div className="navitems">
+                    
+                </div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
