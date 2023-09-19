4f17241b91a9145ab3ac637a31d4412d908857e4
PedroPeriquito <pedro_perikito@hotmail.com>
2023-09-19 12:58:57 +0100

    navbar

---
+++ src/Components/MenuItems/MenuItems.jsx
@@ -0,0 +1,31 @@
+export const MenuItems = [
+	{
+		title: 'Home',
+		url: '/',
+		cName: 'navLinks',
+		icon: 'fa-solid fa-house',
+	},
+	{
+		title: 'Destinations',
+		url: '/Destiantions',
+		cName: 'navLinks',
+		icon: 'fa-solid fa-plane',
+	},
+	{
+		title: 'Booking',
+		url: '/Booking',
+		cName: 'navLinks',
+		icon: 'fa-solid fa-book',
+	},
+	{
+		title: 'Contacts',
+		url: '/Contacts',
+		cName: 'navLinks',
+		icon: 'fa-solid fa-address-book',
+	},
+	{
+		title: 'Sign Up',
+		url: '/signup',
+		cName: 'navLinksMobile',
+	},
+];
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,22 @@
 import { Component } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-
+import { MenuItems } from '../MenuItems/MenuItems';
 class Navbar extends Component {
 	render() {
 		return (
 			<nav className='navbarItems'>
 				<h1 className='navbarLogo'>Nature Adventure</h1>
-				<ul>
-					<li>
-						<Link>
-							<i class='fa-solid fa-house'>Home</i>
-						</Link>
-					</li>
+				<ul className='navMenu'>
+					{MenuItems.map((item, index) => {
+						return (
+							<li key={index}>
+								<a href='/' className={item.cName}>
+									<i class={item.icon}>{item.title}</i>
+								</a>
+							</li>
+						);
+					})}
 				</ul>
 			</nav>
 		);
@@ -20,51 +24,3 @@ class Navbar extends Component {
 }
 
 export default Navbar;
-
-/* import { Link } from 'react-router-dom';
-import './Navbar.css';
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
-const Navbar = () => {
-	const [click, setClick] = useState(false);
-
-	const handleClick = () => setClick(!click);
-	const closeMobileMenu = () => setClick(false);
-	return (
-		<section className='navbar'>
-			<div className='navContainer'>
-				<Link to='/' className='navLogo'>
-					Nature Adventure
-				</Link>
-
-			 	<div className='menuIcon'>
-					<i className='{click ? 'fas fa-times' : <FontAwesomeIcon icon={faBars} />></i>
-
-				
-				<div className='navitems'>
-					<ul className='navList'>
-						<li className='navItem'>
-							<Link to='/' className='navLink'></Link>Home
-						</li>
-						<li className='navItem'>
-							<Link to='/Destinations' className='navLink'></Link>Destinations
-						</li>
-						<li className='navItem'>
-							<Link to='/Booking' className='navLink'></Link>Booking
-						</li>
-						<li className='navItem'>
-							<Link to='/Contacts' className='navLink'></Link>Contacts
-						</li>
-					</ul>
-					<button className='navButton'>Sign Up</button>
-					<button className='navButton'>Login</button>
-				</div>
-			</div>
-		</section>
-	);
-};
-
-export default Navbar;
- */
