8d0d72c87c6e73f84cf9e442470ab2c1afd1ec52
PedroPeriquito <pedro_perikito@hotmail.com>
2023-09-26 16:53:39 +0100

    navbar fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,41 +1,38 @@
-import { Component } from 'react';
 import './Navbar.css';
 import { MenuItems } from '../MenuItems/MenuItems';
 import '../Button/Button.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-class Navbar extends Component {
-	state = { clicked: false };
-	handleClick = () => {
-		this.setState({ clicked: !this.state.clicked });
+const Navbar = () => {
+	const [clicked, setClicked] = useState(false);
+
+	const handleClick = () => {
+		setClicked(!clicked);
 	};
-	render() {
-		return (
-			<nav className='navbarItems'>
-				<h1>
-					<Link to='/' className='navHomeLink'>
-						<i class='fa-solid fa-leaf'></i>Nature Adventure
-					</Link>
-				</h1>
-				<div className='menuIcons' onClick={this.handleClick}>
-					<i className={this.state.clicked ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'}> </i>
-				</div>
-				<ul className={this.state.clicked ? 'navMenu active' : 'navMenu'}>
-					{MenuItems.map((item, index) => {
-						return (
-							<li key={index}>
-								<Link to={item.url} className={item.cName}>
-									<p>{item.title}</p>
-								</Link>
-							</li>
-						);
-					})}
 
-					<button className='navbtn'>Sign Up</button>
-				</ul>
-			</nav>
-		);
-	}
-}
+	return (
+		<nav className='navbarItems'>
+			<h1>
+				<Link to='/' className='navHomeLink'>
+					<i className='fa-solid fa-leaf'></i>Nature Adventure
+				</Link>
+			</h1>
+			<div className='menuIcons' onClick={handleClick}>
+				<i className={clicked ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'}></i>
+			</div>
+			<ul className={clicked ? 'navMenu active' : 'navMenu'}>
+				{MenuItems.map((item, index) => (
+					<li key={index}>
+						<Link to={item.url} className={item.cName}>
+							<p>{item.title}</p>
+						</Link>
+					</li>
+				))}
+				<button className='navbtn'>Sign Up</button>
+			</ul>
+		</nav>
+	);
+};
 
 export default Navbar;
