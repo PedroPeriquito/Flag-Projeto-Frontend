4e28aebe969686a572d0fceb8a2571c6a59de497
PedroPeriquito <pedro_perikito@hotmail.com>
2023-09-28 16:41:16 +0100

    custom hooks on contact

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,25 +1,36 @@
 import './ContactForm.css';
 import { Formik, Form, Field } from 'formik';
 import * as yup from 'yup';
+import useLocalStorage from '../Storage/useLocalStorage';
+import useUpateLogger from '../Storage/useUpdateLogger';
 
 const FormSchema = yup.object().shape({
-	Name: yup.string().required(),
-	Email: yup.string().email().required(),
-	Subject: yup.string().required(),
-	Message: yup.string().required().max(300),
+	name: yup.string().required(),
+	email: yup.string().email().required(),
+	subject: yup.string().required(),
+	message: yup.string().required().max(300),
 });
 
 const ContactForm = () => {
+	const [name, setName] = useLocalStorage('name', '');
+	useUpateLogger(name);
+	const [email, setEmail] = useLocalStorage('email', '');
+	useUpateLogger(email);
+	const [subject, setSubject] = useLocalStorage('subject', '');
+	useUpateLogger(subject);
+	const [message, setMessage] = useLocalStorage('message', '');
+	useUpateLogger(message);
+
 	return (
 		<div className='formContainer'>
 			<h2>Send me a message</h2>
 			<Formik
 				validationSchema={FormSchema} // Use validationSchema instead of validation
 				initialValues={{
-					Name: '',
-					Email: '',
-					Subject: '',
-					Message: '',
+					name: name || '',
+					email: email || '',
+					subject: subject || '',
+					message: message || '',
 				}}
 				onSubmit={values => {
 					// Handle form submission here
@@ -29,24 +40,25 @@ const ContactForm = () => {
 				{({ errors }) => (
 					<Form>
 						<div className='form'>
-							<label htmlFor='Name'>Name:</label>
-							<Field name='Name' type='text' className='formField' />
-							{errors.Name && <p>{errors.Name}</p>}
+							<label htmlFor='name'>Name:</label>
+							<Field name='name' type='text' className='formField' value={name} onChange={e => setName(e.target.value)} />
+							{errors.name && <p>{errors.name}</p>}
+						</div>
+
+						<div className='form'>
+							<label htmlFor='email'>Email:</label>
+							<Field name='email' type='email' className='formField' value={email} onChange={e => setEmail(e.target.value)} />
+							{errors.email && <p>{errors.email}</p>}
 						</div>
 						<div className='form'>
-							<label htmlFor='Email'>Email:</label>
-							<Field name='Email' type='email' className='formField' />
-							{errors.Email && <p>{errors.Email}</p>}
+							<label htmlFor='subject'>Subject:</label>
+							<Field name='subject' type='text' className='formField' value={subject} onChange={e => setSubject(e.target.value)} />
+							{errors.subject && <p>{errors.subject}</p>}
 						</div>
 						<div className='form'>
-							<label htmlFor='Subject'>Subject:</label>
-							<Field name='Subject' type='text' className='formField' />
-							{errors.Subject && <p>{errors.Subject}</p>}
-						</div>
-						<div className='form'>
-							<label htmlFor='Message'>Message:</label>
-							<Field name='Message' as='textarea' rows={4} className='formField' />
-							{errors.Message && <p>{errors.Message}</p>}
+							<label htmlFor='message'>Message:</label>
+							<Field name='message' as='textarea' rows={4} className='formField' value={message} onChange={e => setMessage(e.target.value)} />
+							{errors.message && <p>{errors.message}</p>}
 						</div>
 						<button type='submit'>Send Message</button>
 					</Form>
