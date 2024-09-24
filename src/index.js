631e8bbddfc665531b7a0f69c3d2e7e2ebfa3913
PedroPeriquito <pedro_perikito@hotmail.com>
2024-09-24 15:49:26 +0100

    final fix

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './Components/ChangeMode/ChangeMode';
-import { BlogProvider } from './Components/Blog/BlogContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
 		<ThemeProvider>
-			<BlogProvider>
-				<App />
-			</BlogProvider>
+			<App />
 		</ThemeProvider>
 	</BrowserRouter>,
 );
